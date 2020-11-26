aa9c3eb71a236697a49f5fff5095722fe39469f4
Ana Leticia Souza <analeticiarsouza@gmail.com>
2020-11-25 21:10:08 -0300

    mudanças puramente cosmeticas pq eu sou fresca

--- express-starter/app.js
+++ express-starter/app.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+
+const data = require('./data.json');
+const { setupMaster } = require('cluster');
 
 const app = express();
 
@@ -15,25 +19,43 @@ app.use(bodyParser.json());
 // Routes
 app.post('/*', (req, res) => {
 
+  data['idReserva']++;
+
+  const leftZeros = function(num) {
+    while(num.length < 4)
+      num = "0" + num;
+    return num;
+  }
+
   const transport = nodemailer.createTransport({
     service: 'gmail',
     auth: {
       user: 'hereanagoes@gmail.com',
-      pass: 'bgenirvapnilqgct',
+      pass: '---',
     }
   });
   
   const mailOptions = {
     from: '"ToysRJoy Minions Collection" <hereanagoes@gmail.com>',
-    to: `${JSON.stringify(req.body['email'])} , analeticiarsouza@gmail.com`,
-    subject: 'Reserva efetuada',
+    to: `${req.body['email']} , analeticiarsouza@gmail.com`,
+    subject: `Reserva efetuada - Nº ${leftZeros(data['idReserva'].toString())}`,
     text: `Dados da reserva: \n
-           Nome: ${JSON.stringify(req.body['nome'])} \n
-           E-mail: ${JSON.stringify(req.body['email'])} \n
-           CPF: ${JSON.stringify(req.body['cpf'])} \n
-           Possui diploma universitário: ${JSON.stringify(req.body['diploma'])} \n
-           Produto: ${JSON.stringify(req.body['produto'])}`
+           Nome: ${req.body['nome']} \n
+           E-mail: ${req.body['email']} \n
+           CPF: ${req.body['cpf']} \n
+           Possui diploma universitário: ${req.body['diploma']} \n
+           Produto: ${req.body['produto']}`
   };
+
+  const jsonString = JSON.stringify(data);
+
+  fs.writeFile('./data.json', jsonString, err => {
+      if (err) {
+          console.log('Error writing file', err)
+      } else {
+          console.log('Successfully wrote file')
+      }
+})
   
   transport.sendMail(mailOptions, function(error, info){
     if (error) {
